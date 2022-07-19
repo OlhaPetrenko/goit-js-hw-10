53ded4c8bed603491c5fd8a4f6d6aa2753b3b983
OlhaPetrenko <o.l.petr27@gmail.com>
2022-07-19 03:38:53 +0300

    додає виконане завдання дз

---
+++ src/creatMarkup.js
@@ -0,0 +1,38 @@
+export function creatCountryMarkup(country) {
+  const lang = Object.values(country[0].languages).join(', ');
+  //   console.log(lang);
+  return `<div class="country-card">
+    <img
+        src='${country[0].flags.svg}'
+        alt="country flag"
+        width="60"
+    />
+    <h2 class="country-name">${country[0].name.official}</h2>
+
+</div>
+<p class="country-text">
+    Cтолиця: <span class="country-date">${country[0].capital}</span>
+</p>
+<p class="country-text">
+    Населення: <span class="country-date">${country[0].population}</span>
+</p>
+<p class="country-text">
+   Мова: <span class="country-date">${lang}</span>
+</p>`;
+}
+
+export function creatCountriesMarkup(countries) {
+  return countries
+    .map(country => {
+      return `<li class="country-item">
+    <img
+        src='${country.flags.svg}'
+        alt="country flag"
+        width="40"
+    />
+    <h2 class="country-name">${country.name.official}</h2>
+
+</li>`;
+    })
+    .join('');
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,64 @@
 import './css/styles.css';
+import { fetchCountries } from '../src/fetchCountries';
+import { creatCountryMarkup } from '../src/creatMarkup';
+import { creatCountriesMarkup } from '../src/creatMarkup';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// import templDiv from './templates';
+// import teplateUl from './template/templateUl.hbs';
 
 const DEBOUNCE_DELAY = 300;
+
+const inputEl = document.querySelector('#search-box');
+const countryInfoEl = document.querySelector('.country-info');
+const countryListEl = document.querySelector('.country-list');
+// console.log(countryInfoEl);
+
+inputEl.addEventListener('input', debounce(onInputChangh, 300));
+
+function onInputChangh(event) {
+  //   console.log(event.target.value.trim());
+
+  fetchCountries(event.target.value.trim())
+    .then(data => {
+      console.log(data);
+      if (event.target.value.trim() === '') {
+        countryListEl.innerHTML = '';
+        countryInfoEl.innerHTML = '';
+        return alert('Відсутня інформація для пошуку');
+      }
+      if (data.length > 10) {
+        countryListEl.innerHTML = '';
+        countryInfoEl.innerHTML = '';
+        Notify.info(
+          'Забагато позицій знайдено. Будь ласка, введіть назву країни точніше!'
+        );
+      } else if (data.length > 2 && data.length <= 10) {
+        countryListEl.innerHTML = '';
+        countryInfoEl.innerHTML = '';
+        const countriesMarkup = creatCountriesMarkup(data);
+
+        // console.log(countriesMarkup);
+
+        countryListEl.insertAdjacentHTML('beforeend', countriesMarkup);
+      } else {
+        countryListEl.innerHTML = '';
+        countryInfoEl.innerHTML = '';
+        const countryMarkup = creatCountryMarkup(data);
+
+        // console.log(countryMarkup);
+
+        countryInfoEl.insertAdjacentHTML('beforeend', countryMarkup);
+      }
+    })
+    .catch(err => {
+      if (err.message === '404') {
+        Notify.failure('Упс, немає країни з такою назвою!!!');
+        countryListEl.innerHTML = '';
+        countryInfoEl.innerHTML = '';
+      }
+
+      console.dir(err);
+    });
+}
